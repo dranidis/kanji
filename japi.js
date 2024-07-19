47fb6fb7e875d39be9a380fc01db91a3c88b1903
D Dranidis <dranidis@gmail.com>
2024-07-19 19:31:56 +0300

    Changes

--- japi.js
+++ japi.js
@@ -2,27 +2,55 @@ import JishoAPI from "unofficial-jisho-api";
 
 const jisho = new JishoAPI();
 
-// get the first argument from command line
-const phrase = process.argv[2];
+// Command line options:
+// -u : show alsw uncommon words (default false is to hide them)
+
+const args = process.argv.slice(2);
+
+// Initialize option flags
+let showUncommon = false;
+let showWordsOnly = false;
+
+// Parse command-line options
+args.forEach(arg => {
+  if (arg === '-u') {
+    showUncommon = true;
+  }
+  if (arg === '-w') {
+    showWordsOnly = true;
+  }
+});
+
+// Get the first argument that is not an option as the phrase
+const phrase = args.find(arg => !arg.startsWith('-'));
 
 jisho.searchForPhrase(phrase).then((result) => {
   if (!result.data) return;
 
+  console.log(result.data);
+
   result.data.forEach((entry) => {
-    // if (!entry.is_common) return;
+    if (!entry.is_common && !showUncommon) return;
 
     var output = "";
 
     entry.japanese.forEach((japanese) => {
-      output += japanese.word + "\t" + japanese.reading + "\n";
+      if (showWordsOnly) {
+        output += japanese.word + " ";
+      } else {
+        output += japanese.word + "\t" + japanese.reading + "\n";
+      }
     });
 
-    if (entry.jlpt.length > 0) output += entry.jlpt.join(", ") + "\n";
-    if (!entry.is_common) output += "UC\n";
+    if (!showWordsOnly && entry.jlpt.length > 0) output += entry.jlpt.join(", ") + "\n";
+    if (!showWordsOnly && !entry.is_common) output += "UC\n";
 
-    entry.senses.forEach((sense, index) => {
-      output += index + 1 + ".\t" + sense.english_definitions.join(", ") + "\n";
-    });
+    if (!showWordsOnly) {
+      entry.senses.forEach((sense, index) => {
+        output += index + 1 + ".\t" + sense.english_definitions.join(", ") + "\n";
+      });
+    }
+
     output += "\n";
 
     console.log(output);
